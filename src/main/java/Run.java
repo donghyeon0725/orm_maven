30e2d97512a2ae38d6b7110f6ac2b09e0c0f6d5b
김동현 <donghyeon@liveklass.com>
2022-03-18 20:33:45 +0900

    jpa default

---
+++ src/main/java/Run.java
@@ -0,0 +1,105 @@
+import entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class Run {
+    public static void main(String[] args) {
+//        Run.run1();
+//        Run.run2();
+//        Run.run3();
+//        Run.jpql1();
+        Run.jpql2();
+    }
+
+    public static void run1() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("HelloA");
+        entityManager.persist(member);
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    public static void run2() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        entityTransaction.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("HelloA");
+        entityManager.persist(member);
+
+        entityTransaction.commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    public static void run3() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        entityTransaction.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+            entityManager.persist(member);
+
+            entityTransaction.commit();
+        } catch (Exception e) {
+            entityTransaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+
+    public static void jpql1() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        List<Member> memberList = entityManager.createQuery("select m from Member as m", Member.class).getResultList();
+
+        for (Member member : memberList) {
+            System.out.println("name : " + member.getName());
+        }
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    public static void jpql2() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        List<Member> memberList = entityManager.createQuery("select m from Member as m", Member.class)
+                .setFirstResult(0)
+                .setMaxResults(1)
+                .getResultList();
+
+        for (Member member : memberList) {
+            System.out.println("name : " + member.getName());
+        }
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+}
